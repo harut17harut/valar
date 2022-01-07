889a18364d493ddb1fdeeea30f27143d98cd34b1
gencher <36341428+gencher@users.noreply.github.com>
2022-01-07 17:24:09 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+const app = express();
+import path from  'path';
+import  {routes} from  './routes/index.js';
+import expressLayouts from 'express-ejs-layouts';
+
+const port = process.env.PORT || 8080;
+const __dirname = path.resolve(path.dirname(''));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.use(express.urlencoded({ extended: true }));
+
+app.set('layout', 'layouts/blank');
+
+app.use(express.static('public'));
+
+app.use('/', routes);
+
+app.listen(port, () => {
+    console.log(`started: localhost:${port}`)
+})
